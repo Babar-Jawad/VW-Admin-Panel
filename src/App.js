2c363ace347f515bf304a002cef6b4b5ec79d41c
Babar-Jawad <babarjawad010@gmail.com>
2023-04-25 09:35:00 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Advocates from "./Pages/Advocates/Advocates";
 import Login from "./Pages/Login/Login";
 import Users from "./Pages/Users/Users";
 import Documents from "./Pages/Documents/Documents";
-import previousCases from "./Pages/previousCases/previousCases";
+// import previousCases from "./Pages/previousCases/previousCases";
 import MainDash from "./components/MainDash/MainDash";
 import Sidebar from "./components/Sidebar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
